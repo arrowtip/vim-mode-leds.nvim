d47091d475e9cc2c17f8f6e3fe1839bed0775dfa
Jakob Schmid <jakobschmid25.12@gmail.com>
2023-09-20 18:16:16 +0200

    refactor code for better readability

--- lua/vim-mode-leds/led-control.lua
+++ lua/vim-mode-leds/led-control.lua
@@ -7,13 +7,7 @@ end
 vim.api.nvim_create_user_command(
   'LedMode',
   function(opts)
-    print("LED mode: " .. opts.fargs[1])
-    if opts.fargs[1] ~= '0' then
-      print("storing value "..opts.fargs[1])
-      vim.api.nvim_win_set_var(0, 'LedMode', opts.fargs[1])
-    end
     set_led(opts.fargs[1])
-    print(vim.api.nvim_win_get_var(0, 'LedMode'))
   end,
   {
     nargs = 1,
--- lua/vim-mode-leds/mode-detection.lua
+++ lua/vim-mode-leds/mode-detection.lua
@@ -3,43 +3,64 @@ local group_id = vim.api.nvim_create_augroup("vim-mode-leds", { clear = true })
 vim.api.nvim_create_autocmd("ModeChanged", {
   pattern = { '*:n' },
   group = group_id,
-  command = 'LedMode 1'
+  callback = function ()
+    vim.cmd('LedMode 1')
+    vim.api.nvim_win_set_var(0, 'VimModeLeds_ModeStore', 1)
+  end
 })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
   pattern = { '*:v' },
   group = group_id,
-  command = 'LedMode 2'
+  callback = function ()
+    vim.cmd('LedMode 2')
+    vim.api.nvim_win_set_var(0, 'VimModeLeds_ModeStore', 2)
+  end
 })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
   pattern = { '*:s' },
   group = group_id,
-  command = 'LedMode 3'
+  callback = function ()
+    vim.cmd('LedMode 3')
+    vim.api.nvim_win_set_var(0, 'VimModeLeds_ModeStore', 3)
+  end
 })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
   pattern = { '*:i' },
   group = group_id,
-  command = 'LedMode 4'
+  callback = function ()
+    vim.cmd('LedMode 4')
+    vim.api.nvim_win_set_var(0, 'VimModeLeds_ModeStore', 4)
+  end
 })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
   pattern = { '*:c' },
   group = group_id,
-  command = 'LedMode 5'
+  callback = function ()
+    vim.cmd('LedMode 5')
+    vim.api.nvim_win_set_var(0, 'VimModeLeds_ModeStore', 5)
+  end
 })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
   pattern = { '*:cv' },
   group = group_id,
-  command = 'LedMode 5'
+  callback = function ()
+    vim.cmd('LedMode 6')
+    vim.api.nvim_win_set_var(0, 'VimModeLeds_ModeStore', 6)
+  end
 })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
   pattern = { '*:t' },
   group = group_id,
-  command = 'LedMode 6'
+  callback = function ()
+    vim.cmd('LedMode 7')
+    vim.api.nvim_win_set_var(0, 'VimModeLeds_ModeStore', 7)
+  end
 })
 
 vim.api.nvim_create_autocmd({"FocusLost", "VimLeave", "VimSuspend"}, {
@@ -51,8 +72,8 @@ vim.api.nvim_create_autocmd({"FocusLost", "VimLeave", "VimSuspend"}, {
 vim.api.nvim_create_autocmd("FocusGained", {
   pattern = { '*' },
   group = group_id,
-  callback = function(args)
-    print("stored mode is: "..vim.api.nvim_win_get_var(0, 'LedMode'))
-    vim.cmd("LedMode "..vim.api.nvim_win_get_var(0, 'LedMode'))
+  -- command = 'LedMode'..vim.api.nvim_win_get_var(0, 'led_mode')
+  callback = function ()
+    vim.cmd('LedMode'..vim.api.nvim_win_get_var(0, 'VimModeLeds_ModeStore'))
   end
 })
