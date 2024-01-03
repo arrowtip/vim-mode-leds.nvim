96ba24cd4a41435b36fbed427f2f2b9c247540ba
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-01-03 11:11:54 +0100

    restructure to propper plugin structure

--- lua/vim-mode-leds/init.lua
+++ lua/vim-mode-leds/init.lua
@@ -1,3 +1,30 @@
-print("im working")
-require('vim-mode-leds.led-control')
+local M = {}
+local path_to_leds
+
+print("hello from lua init")
+function M.setup(opts)
+  opts = opts or {}
+  if opts.path_to_leds then
+    path_to_leds = opts.path_to_leds
+  else
+    M.path_to_leds = '/dev/ttyACM0'
+  end
+end
+
 require('vim-mode-leds.mode-detection')
+
+local function set_led(chr)
+  local job = vim.fn.jobstart('echo "' .. chr .. '" > ' .. M.path_to_leds)
+end
+
+vim.api.nvim_create_user_command(
+  'LedMode',
+  function(opts)
+    set_led(opts.fargs[1])
+  end,
+  {
+    nargs = 1,
+    desc = "Set LEDs to given mode"
+  }
+)
+return M
---
+++ plugin/vim-mode-leds.lua
@@ -0,0 +1,1 @@
+print("hello from plugin/vim-mode-leds")
